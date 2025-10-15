3b5bcec81abdc5d9c4bbd09fdd5c2667645d5139
Amin Ghasemi <amin11.ghasemi@gmail.com>
2025-10-15 16:28:14 +0330

    Use Lombok to generate getter and setters

--- src/main/java/com/amin/store/entities/User.java
+++ src/main/java/com/amin/store/entities/User.java
@@ -1,8 +1,14 @@
 package com.amin.store.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Setter
+@Getter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Table(name = "users")
 public class User {
     @Id
@@ -14,39 +20,8 @@ public class User {
 
     @Column(nullable = false, name = "email")
     private String email;
-    
+
     @Column(nullable = false, name = "password")
     private String password;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
