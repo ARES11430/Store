4986273b0c70dd460cf39a50d1452c200ca4beae
Amin Ghasemi <amin11.ghasemi@gmail.com>
2025-10-17 14:15:23 +0330

    Using Repositories

--- src/main/java/com/amin/store/StoreApplication.java
+++ src/main/java/com/amin/store/StoreApplication.java
@@ -1,9 +1,7 @@
 package com.amin.store;
 
-import com.amin.store.entities.Address;
-import com.amin.store.entities.Profile;
-import com.amin.store.entities.Tag;
 import com.amin.store.entities.User;
+import com.amin.store.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,28 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreApplication {
 
     public static void main(String[] args) {
-        //var context = SpringApplication.run(StoreApplication.class, args);
+        var context = SpringApplication.run(StoreApplication.class, args);
+        var userRepository = context.getBean(UserRepository.class);
 
         var user = User.builder()
-                .id(1L)
                 .name("admin")
                 .email("admin@google.com")
                 .password("admin").build();
 
-
-        var profile = Profile.builder().bio("bio").build();
-        user.setProfile(profile);
-        profile.setUser(user);
-
-        System.out.println("--- User Object ---");
-        System.out.println(user);
-
-        System.out.println("\n--- Profile Object (toString() output) ---");
-        System.out.println(profile);
-
-        System.out.println("\n--- Verifying the link from Profile back to User ---");
-        // This proves the connection exists, even if toString() hides it
-        System.out.println("Username from profile object: " + profile.getUser().getName());
+        userRepository.save(user);
     }
-
 }
